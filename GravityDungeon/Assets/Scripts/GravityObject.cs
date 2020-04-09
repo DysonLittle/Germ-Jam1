441318494f3b685bdb175fe7f6773996e82d67d1
Dyson Little <dyson.little@mymail.champlain.edu>
2020-04-09 16:00:12 -0400

    Added test scene and some scripts for gravity and dungeon building

---
+++ GravityDungeon/Assets/Scripts/GravityObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityObject : MonoBehaviour
+{
+    public Vector3 gravityDirection = Vector3.zero;
+    public const float g = 9.8f;
+    private void FixedUpdate()
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(rb.mass * gravityDirection * g);
+    }
+}
---
+++ GravityDungeon/Assets/Scripts/SimpleWallBuilderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleWallBuilderScript : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public Vector3 dimentions;
+    void Start()
+    {
+        for (int i = 0; i < dimentions.x; i++)
+        {
+            for (int j = 0; j < dimentions.y; j++)
+            {
+                for (int k = 0; k < dimentions.z; k++)
+                {
+                    if (i != 0 && i != dimentions.x - 1 &&
+                        j != 0 && j != dimentions.y - 1 &&
+                        k != 0 && k != dimentions.z - 1)
+                        continue;
+                    Instantiate(wallPrefab, transform.position + new Vector3(i, j, k), Quaternion.identity, gameObject.transform);
+                }
+            }
+        }
+    }
+}
