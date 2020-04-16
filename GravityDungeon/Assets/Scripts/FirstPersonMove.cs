ab0cd12f5eb790ed0daa770f9c426ea209462b3d
Quincy Buchanan <quincyb100@gmail.com>
2020-04-16 14:48:38 -0400

    Some work on the first person demo

---
+++ GravityDungeon/Assets/Scripts/FirstPersonMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMove : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [SerializeField]
+    float moveSpeed;
+
+    [SerializeField]
+    float sprintSpeedMultiplier = 1.5f;
+
+    [SerializeField]
+    float jumpHeight = 3f;
+
+    public float _gravity = -10f;
+
+    private float _yAxisVelocity;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            vertical *= sprintSpeedMultiplier;
+
+        Vector3 movement = horizontal * moveSpeed * Time.deltaTime * transform.right +
+                           vertical * moveSpeed * Time.deltaTime * transform.forward;
+
+        if (characterController.isGrounded)
+            _yAxisVelocity = -0.5f;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+            _yAxisVelocity = Mathf.Sqrt(jumpHeight * -2f * _gravity);
+
+        _yAxisVelocity += _gravity * Time.deltaTime;
+        movement.y = _yAxisVelocity * Time.deltaTime;
+
+        characterController.Move(movement);
+    }
+}
---
+++ GravityDungeon/Assets/Scripts/Raycast.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour
+{
+    private GameObject raycastedObj;
+
+    [SerializeField]
+    GameObject interactionType;
+
+    [SerializeField]
+    int rayLength = 10;
+
+    [SerializeField]
+    LayerMask layerMask;
+
+    [SerializeField]
+    Image crosshair;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMask.value))
+        {
+            if(hit.collider.CompareTag("Physics"))
+            {
+                raycastedObj = hit.collider.gameObject;
+                CrosshairActive();
+
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    raycastedObj.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            CrosshairNormal();
+        }
+    }
+
+    void CrosshairActive()
+    {
+        crosshair.color = Color.green;
+    }
+
+    void CrosshairNormal()
+    {
+        crosshair.color = Color.white;
+    }
+}
