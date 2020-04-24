0ec652ce60b9b3d58eab9e6fb0249260d93fc393
Dyson Little <dyson.little@mymail.champlain.edu>
2020-04-23 22:52:01 -0400

    Improved demo

---
+++ GravityDungeon/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float cameraSpacingDefault;
+    float cameraSpacing;
+    public float cameraSpacingMin;
+
+    Quaternion cameraRotation;
+
+    public Vector3 sensitivity;
+
+    Rigidbody rb;
+
+    PlayerScript player;
+    Transform cameraObj;
+
+    void Start()
+    {
+        player = GameObject.Find("PlayerCharacter").GetComponent<PlayerScript>();
+        cameraObj = transform.GetChild(0);
+        rb = GetComponent<Rigidbody>();
+        cameraSpacing = cameraSpacingDefault;
+        cameraRotation = Quaternion.identity;
+        Cursor.visible = false;
+    }
+
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        cameraRotation = Quaternion.AngleAxis(mouseX * sensitivity.x, Vector3.up) * cameraRotation * Quaternion.AngleAxis(mouseY * sensitivity.y, Vector3.left);
+
+        
+        rb.MoveRotation(cameraRotation);
+        player.forwardVec = new Vector3(Mathf.Sin(Mathf.Deg2Rad * cameraRotation.eulerAngles.y), 0f, Mathf.Cos(Mathf.Deg2Rad * cameraRotation.eulerAngles.y));
+        player.rightVec = Quaternion.AngleAxis(90, Vector3.up) * player.forwardVec;
+        
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        cameraSpacing += scroll * sensitivity.z;
+        if (cameraSpacing >= -cameraSpacingMin)
+            cameraSpacing = -cameraSpacingMin;
+
+        cameraObj.localPosition = new Vector3(0f, 0f, cameraSpacing);
+        
+    }
+}
--- GravityDungeon/Assets/Scripts/PlayerScript.cs
+++ GravityDungeon/Assets/Scripts/PlayerScript.cs
@@ -12,7 +12,6 @@ public class PlayerScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //rb.drag = moveAccl / termVel;
     }
 
     void Update()
@@ -24,13 +23,49 @@ public class PlayerScript : MonoBehaviour
         CheckMove();
     }
 
-    void CheckMove()
+    void CheckMoveDef()
+    {
+        //Vector3 moveDir = forwardVec * Input.GetAxis("Vertical") + rightVec * Input.GetAxis("Horizontal");
+        //float nonGravSpeedSq = Vector3.ProjectOnPlane(rb.velocity, GetGravVector()).sqrMagnitude;
+        //float dragMultiplier = (termVel * termVel - nonGravSpeedSq) / (termVel * termVel);
+        //rb.AddForce(moveDir.normalized * moveAccl * rb.mass * dragMultiplier);
+        Vector3 moveDir = forwardVec * Input.GetAxis("Vertical") + rightVec * Input.GetAxis("Horizontal");
+        rb.AddForce(moveDir.normalized * moveAccl * rb.mass);
+
+        Vector3 horizontalVel = Vector3.ProjectOnPlane(rb.velocity, GetGravVector());
+        if (horizontalVel.sqrMagnitude > termVel * termVel)
+        {
+            rb.velocity = Vector3.Project(rb.velocity, GetGravVector()) + (horizontalVel.normalized * termVel);
+        }
+    }
+
+    void CheckMoveDef2()
     {
         Vector3 moveDir = forwardVec * Input.GetAxis("Vertical") + rightVec * Input.GetAxis("Horizontal");
-        
-        rb.AddForce(moveDir.normalized * moveAccl * rb.mass);
-        float nonGravSpeedSq = Vector3.ProjectOnPlane(rb.velocity, GetGravVector()).sqrMagnitude;
+        Vector3 verticalComponent = Vector3.Project(rb.velocity, GetGravVector());
+        //Vector3 horizontalComponent = Vector3.ProjectOnPlane(rb.velocity, GetGravVector());
+        Vector3 horizontalComponent = moveDir.normalized * termVel;
+        rb.velocity = verticalComponent + horizontalComponent;
 
+    }
+
+    void CheckMove()
+    {
+        Vector3 moveDir = (forwardVec * Input.GetAxis("Vertical") + rightVec * Input.GetAxis("Horizontal")).normalized;
+        
+
+        Vector3 horizontalComponent = Vector3.ProjectOnPlane(rb.velocity, GetGravVector());
+        float moveEffectiveness = (termVel - horizontalComponent.magnitude) / termVel;
+        if (moveEffectiveness < 0)
+            moveEffectiveness = 0;
+        Vector3 parallelComponent = Vector3.Project(moveDir, horizontalComponent);
+        Vector3 perpendicularComponent = moveDir - parallelComponent;
+        if (Vector3.Dot(parallelComponent, horizontalComponent) >= 0)
+        {
+            parallelComponent *= moveEffectiveness;
+        }
+
+        rb.AddForce((parallelComponent + perpendicularComponent) * moveAccl * rb.mass);
     }
 
     Vector3 GetGravVector()
