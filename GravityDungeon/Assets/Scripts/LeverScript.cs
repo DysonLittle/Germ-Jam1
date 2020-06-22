7f0c26bac9b317e5c8dd2930758b797ee0395c27
Dyson Little <dyson.little@mymail.champlain.edu>
2020-06-22 03:10:45 -0400

    Added first pass of interactable interface

---
+++ GravityDungeon/Assets/Scripts/LeverScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : Interactable
+{
+    GameObject canvas;
+    public GameObject hoverPromptPrefab;
+    GameObject hoverPromptObj;
+    bool hovering = false;
+
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+
+    private void Update()
+    {
+        if (hovering)
+        {
+            hoverPromptObj.transform.position = GetCanvasSpaceFromWorldSpace(transform.position);
+        }
+    }
+
+    public override void Hover(bool toggle = true)
+    {
+        hovering = toggle;
+        if (toggle)
+        {
+            hoverPromptObj = Instantiate(hoverPromptPrefab, canvas.transform);
+            hoverPromptObj.transform.position = GetCanvasSpaceFromWorldSpace(transform.position);
+        }
+        else
+        {
+            Destroy(hoverPromptObj);
+        }
+    }
+
+    public override void Interact(GameObject player)
+    {
+        Debug.Log("Pull the lever, Kronk!");
+        player.GetComponent<PlayerInteractionScript>().currentInteractableSelected = null;
+    }
+
+    Vector3 GetCanvasSpaceFromWorldSpace(Vector3 worldSpace)
+    {
+        Vector3 viewSpaceVec = Camera.main.WorldToViewportPoint(worldSpace);
+        Vector2 sizeDelta = canvas.GetComponent<RectTransform>().sizeDelta;
+        return new Vector3(viewSpaceVec.x * sizeDelta.x, viewSpaceVec.y * sizeDelta.y, 0);
+    }
+}
---
+++ GravityDungeon/Assets/Scripts/PlayerInteractionScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionScript : MonoBehaviour
+{
+    SphereCollider sc;
+    Interactable currentInteractableHover;
+    public Interactable currentInteractableSelected;
+    void Start()
+    {
+        sc = GetComponents<SphereCollider>()[1]; //get the second, larger one, used for interactable objects
+        currentInteractableHover = null;
+        currentInteractableSelected = null;
+    }
+
+    void Update()
+    {
+        if (!currentInteractableSelected)
+            FindInteractables();
+        CheckSelection();
+    }
+
+    void CheckSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractableHover)
+        {
+            currentInteractableSelected = currentInteractableHover;
+            UpdateCurrentInteractableHover(null);
+            currentInteractableSelected.Interact(gameObject);
+            
+        }
+    }
+
+    void FindInteractables()
+    {
+        Collider[] colliderHits = Physics.OverlapSphere(sc.center + transform.position, sc.radius);
+        
+        if (colliderHits.Length == 0) //no gameObjects in sphere
+        {
+            UpdateCurrentInteractableHover(null);
+            return;
+        }
+
+        GameObject closestInteractable = null;
+        for (int i = 0; i < colliderHits.Length; i++)
+        {
+            GameObject currentObj = colliderHits[i].gameObject;
+            if (currentObj.GetComponent<Interactable>())
+            {
+                if (!closestInteractable)
+                {
+                    closestInteractable = currentObj;
+                }
+                else if((currentObj.transform.position - sc.center - transform.position).sqrMagnitude < (closestInteractable.transform.position - sc.center - transform.position).sqrMagnitude)
+                {
+                    closestInteractable = currentObj;
+                }
+            }
+        }
+
+        if (!closestInteractable) //no interactables found
+        {
+            UpdateCurrentInteractableHover(null);
+            return;
+        }
+
+        UpdateCurrentInteractableHover(closestInteractable.GetComponent<Interactable>());
+    }
+
+    void UpdateCurrentInteractableHover(Interactable newInteractable)
+    {
+        if (newInteractable == currentInteractableHover)
+            return;
+
+        if (currentInteractableHover)
+        {
+            currentInteractableHover.Hover(false);
+        }
+        currentInteractableHover = newInteractable;
+        if (currentInteractableHover)
+        {
+            currentInteractableHover.Hover();
+        }
+    }
+}
