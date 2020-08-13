4f95b3a732e60c9359a86792a47d30916222dd68
Dyson Little <dyson.little@mymail.champlain.edu>
2020-08-13 16:42:02 -0400

    Added title screen

---
+++ GravityDungeon/Assets/Scripts/TitleScreenGimmikScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenGimmikScript : MonoBehaviour
+{
+    Quaternion worldRotation;
+    public Vector2 sensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        worldRotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity.x;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity.y;
+
+        Vector3 eulerRot = new Vector3(mouseY, 0, mouseX);
+
+
+        worldRotation = worldRotation * Quaternion.Euler(eulerRot);
+        //GetComponent<LineRenderer>().SetPosition(1, (worldRotation * Vector3.down) * 10);
+        UpdateAllGravityObjects();
+    }
+
+    void UpdateAllGravityObjects()
+    {
+        foreach (GravityObject i in GravityObject.gravityObjectList)
+        {
+            if (i.followCameraGravity)
+            {
+                i.gravityDirection = worldRotation * Vector3.down;
+            }
+        }
+    }
+}
---
+++ GravityDungeon/Assets/Scripts/TitleScreenMenuScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenMenuScript : MonoBehaviour
+{
+    public GameObject titleScreenStart, levelSelect, instructions, credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("PetraPuzzleTest1");
+    }
+
+    public void LevelSelectButton()
+    {
+
+    }
+
+    public void InstructionsButton()
+    {
+        titleScreenStart.SetActive(false);
+        instructions.SetActive(true);
+    }
+
+    public void CreditsButton()
+    {
+
+    }
+
+    public void BackButton()
+    {
+        titleScreenStart.SetActive(true);
+        //levelSelect.SetActive(false);
+        instructions.SetActive(false);
+        //credits.SetActive(false);
+    }
+}
