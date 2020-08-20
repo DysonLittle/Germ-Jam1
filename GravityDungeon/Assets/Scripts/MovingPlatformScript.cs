78af034892f2a4e153bf7cd91b0d90eda98f9a54
Dyson Little <dyson.little@mymail.champlain.edu>
2020-08-19 20:07:55 -0400

    Added moving platforms

---
+++ GravityDungeon/Assets/Scripts/MovingPlatformScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformScript : Toggleable
+{
+    public float lerpTotalTime;
+    float lerpTimeElapsed = 0.0f;
+    Vector3 offPosition, onPosition;
+    bool toggle = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offPosition = transform.Find("OffPosition").position;
+        onPosition = transform.Find("OnPosition").position;
+        transform.position = offPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toggle && transform.position != onPosition)
+        {
+            lerpTimeElapsed += Time.deltaTime;
+            if (lerpTimeElapsed > lerpTotalTime)
+                lerpTimeElapsed = lerpTotalTime;
+            transform.position = Vector3.Lerp(offPosition, onPosition, lerpTimeElapsed / lerpTotalTime);
+        }
+        else if (!toggle && transform.position != offPosition)
+        {
+            lerpTimeElapsed -= Time.deltaTime;
+            if (lerpTimeElapsed < 0.0f)
+                lerpTimeElapsed = 0.0f;
+            transform.position = Vector3.Lerp(offPosition, onPosition, lerpTimeElapsed / lerpTotalTime);
+        }
+        
+    }
+
+    public override void Toggle(bool toggleState)
+    {
+        toggle = toggleState;
+    }
+
+}
---
+++ GravityDungeon/Assets/Scripts/MovingPlatformScriptAlt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformScriptAlt : Toggleable
+{
+    public float lerpTotalTime;
+    float lerpTimeElapsed = 0.0f;
+    Vector3 offPosition, onPosition;
+    bool movingForward = true;
+    bool moving = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offPosition = transform.Find("OffPosition").position;
+        onPosition = transform.Find("OnPosition").position;
+        transform.position = offPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            if (movingForward)
+            {
+                lerpTimeElapsed += Time.deltaTime;
+                if (lerpTimeElapsed >= lerpTotalTime)
+                {
+                    lerpTimeElapsed = lerpTotalTime - (lerpTimeElapsed - lerpTotalTime); //written like this for clarity. When we overshoot the end, reflect us back appropriately
+                    movingForward = false;
+                }
+            }
+            else
+            {
+                lerpTimeElapsed -= Time.deltaTime;
+                if (lerpTimeElapsed <= 0.0f)
+                {
+                    lerpTimeElapsed *= -1.0f; //When we overshoot the beginning, reflect us back appropriately
+                    movingForward = true;
+                }
+                
+            }
+            transform.position = Vector3.Lerp(offPosition, onPosition, lerpTimeElapsed / lerpTotalTime);
+        }
+    }
+
+    public override void Toggle(bool toggleState)
+    {
+        moving = toggleState;
+    }
+
+}
